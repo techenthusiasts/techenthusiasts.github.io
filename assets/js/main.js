de56f6a8641bfdce8cf1b21c003526608e26ed99
Roy Louis Garcia <roygarcia201@gmail.com>
2019-09-23 10:23:04 -0700

    added an show email button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
     $('.u-email').on('click', function (event) {
         event.preventDefault();
         $(this).off("click");
+        var img = document.createElement("img");
+        img.src = "/assets/images/posts_images/btn-email.png";
+        img.setAttribute('class', 'email-btn');
+        var src = document.getElementById("invi-img");
+        src.appendChild(img);
         var email = $(this).attr("data-email").replace(/AT/, '@').replace(/DOT/, '.');
         $(this).removeClass("u-email");
+        $(this).addClass("email-link");
         $(this).html(email);
         $(this).attr("href", "mailto:" + email);
     });
